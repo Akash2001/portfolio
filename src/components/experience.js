5900d229338eb98387b25d555a4ebce449743c58
Akash2001 <akashngadhave@gmail.com>
2024-03-25 13:33:30 +0530

    Experience : Details made collapsible.

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,26 +1,37 @@
+import { useState } from "react"
 import { experiences } from "../data"
 
 export const Experience = () => {
+    const [areDetailsCollapsed, setAreDetailsCollapsed] = useState(Array.from({ length: experiences.length }, (_, i) => true));
+    
+    const handleToggleCollapse = (index) => {
+        let temp = [...areDetailsCollapsed];
+        temp[index] = !temp[index];
+        setAreDetailsCollapsed(temp);
+    }
+
     return (
         <div id="mySkills">
             Experience : <br />
             {
                 experiences.map((experience, i) => {
                     return (
-                        <div key={i}>
-                            <div className="bold-txt">
-                                <div>{experience.org} - {experience.role}</div>
+                        <div className="mb-2" key={i}>
+                            <div className="mb-1 cursor-pointer" onClick={(e)=> handleToggleCollapse(i)}>
+                                <div className="bold-txt">
+                                    <div>{experience.org} - {experience.role}</div>
+                                </div>
+                                <div className="text-small bold-txt">
+                                    {experience.location}
+                                    {' | '}
+                                    {experience.duration}
+                                </div>
                             </div>
-                            <div className="text-small bold-txt">
-                                {experience.location}
-                                {' | '}
-                                {experience.duration}
-                            </div>
-                            <ul>
+                            {!areDetailsCollapsed[i] ? (<ul>
                                 {experience.details.map((item, j) => {
                                     return <li key={i + j}>{item}</li>
                                 })}
-                            </ul>
+                            </ul>) : (<></>)}
                         </div>
                     )
                 })
