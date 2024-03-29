476fd62d6c478b2ccc53af8a6fa8eb68a8e4a121
Akash2001 <akashngadhave@gmail.com>
2024-03-30 00:00:14 +0530

    Gallery modal component added.

---
+++ src/common/modal.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Modal } from "@mui/material";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
+export const ModalComponent = ({ isOpen, setIsOpen, images, title }) => {
+  const [index, setIndex] = useState(0);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleImageChange = (e, isRight = false) => {
+    if (isRight && index < images.length - 1) {
+      setIndex(index + 1);
+    } else if (isRight === false && index > 0) {
+      setIndex(index - 1);
+    }
+  };
+
+  return (
+    <Modal
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div className="flex items-center justify-center h-full">
+        <div className="bg-[#fff] p-4 rounded-md">
+          <div className="flex justify-end">
+            <IoClose className="w-8 h-8 cursor-pointer" onClick={handleClose} />
+          </div>
+          <div className="text-center mb-4 mt-2 text-lg font-bold">{title}</div>
+          <div className="flex gap-4">
+            <div className="flex items-center">
+              <FaAngleLeft
+                className="w-8 h-8 cursor-pointer"
+                onClick={handleImageChange}
+              />
+            </div>
+            <div>
+              <img
+                className="w-[60vw] h-[70vh] rounded-md"
+                src={images[index].img}
+                alt="displayed_image"
+              />
+              <div className="text-center mt-4">{images[index].caption}</div>
+            </div>
+            <div className="flex items-center">
+              <FaAngleRight
+                className="w-8 h-8 cursor-pointer"
+                onClick={(e) => handleImageChange(e, true)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
